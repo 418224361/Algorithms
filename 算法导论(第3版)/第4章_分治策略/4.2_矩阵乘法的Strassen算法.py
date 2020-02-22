d51b4ce7c6ff0295e534e3d58fb1cb363c68a0f5
zhaopeng <418224361@qq.com>
2020-02-22 18:09:06 +0800

    4.2增加Strassen方法计算矩阵乘积

--- 算法导论(第3版)/第4章_分治策略/4.2_矩阵乘法的Strassen算法.py
+++ 算法导论(第3版)/第4章_分治策略/4.2_矩阵乘法的Strassen算法.py
@@ -31,10 +31,57 @@ def check_matrix(matrix, empty_matrix=False):
     return True
 
 
-def matrix_multipy(A, B):
-    # 检查矩阵非空，各个位置均有元素填充
-    check_matrix(A, empty_matrix=False)
-    check_matrix(A, empty_matrix=False)
+def matrix_minus(A, B, check=False):
+    if check is True:
+        # 检查矩阵非空，各个位置均有元素填充
+        check_matrix(A, empty_matrix=False)
+        check_matrix(B, empty_matrix=False)
+
+    # 获取矩阵规模
+    a_row_number, a_column_number = get_matrix_size(A)
+    b_row_number, b_column_number = get_matrix_size(B)
+
+    # 检查A的行列数是否等于B的行列数
+    if a_column_number != b_column_number or a_row_number != b_row_number:
+        raise ValueError('{}的行列不等于{}的行列数'.format(A, B))
+
+    # 计算矩阵加法
+    temp_array = [0] * a_column_number
+    matrix = [temp_array[:] for t in range(a_row_number)]
+    for i in range(a_row_number):
+        for j in range(a_column_number):
+            matrix[i][j] = A[i][j] - B[i][j]
+    return matrix
+
+
+def matrix_add(A, B, check=False):
+    if check is True:
+        # 检查矩阵非空，各个位置均有元素填充
+        check_matrix(A, empty_matrix=False)
+        check_matrix(B, empty_matrix=False)
+
+    # 获取矩阵规模
+    a_row_number, a_column_number = get_matrix_size(A)
+    b_row_number, b_column_number = get_matrix_size(B)
+
+    # 检查A的行列数是否等于B的行列数
+    if a_column_number != b_column_number or a_row_number != b_row_number:
+        raise ValueError('{}的行列不等于{}的行列数'.format(A, B))
+
+    # 计算矩阵加法
+    temp_array = [0] * a_column_number
+    matrix = [temp_array[:] for t in range(a_row_number)]
+    for i in range(a_row_number):
+        for j in range(a_column_number):
+            matrix[i][j] = A[i][j] + B[i][j]
+    return matrix
+
+
+def matrix_multiply(A, B, check=False):
+    if check is True:
+        # 检查矩阵非空，各个位置均有元素填充
+        check_matrix(A, empty_matrix=False)
+        check_matrix(B, empty_matrix=False)
 
     # 获取矩阵规模
     a_row_number, a_column_number = get_matrix_size(A)
@@ -57,14 +104,15 @@ def matrix_multipy(A, B):
     return matrix
 
 
-def matrix_fillup(matrix):
+def matrix_fillup(matrix, check=True):
     """
     对任意矩阵进行检查，并用0补齐为n阶方阵，其中n是2的整数次幂。
     :param matrix: 待补齐的矩阵
     :return: 补齐后的方阵, 原始行数，原始列数，n
     """
-    # 检查矩阵非空，各个位置均有元素填充
-    check_matrix(matrix, empty_matrix=False)
+    if check is True:
+        # 检查矩阵非空，各个位置均有元素填充
+        check_matrix(matrix, empty_matrix=False)
 
     # 计算目标行列数n
     row_number, columum_number = get_matrix_size(matrix)
@@ -85,8 +133,8 @@ def matrix_fillup(matrix):
 
 def sub_matrix(matrix):
     matrix, row_number, columum_number, n = matrix_fillup(matrix)
-    half_n = int(n/2)
-    temp_array = [0] * int(n/2)
+    half_n = int(n / 2)
+    temp_array = [0] * int(n / 2)
     m11 = [temp_array[:] for t in range(half_n)]
     m12 = [temp_array[:] for t in range(half_n)]
     m21 = [temp_array[:] for t in range(half_n)]
@@ -97,22 +145,81 @@ def sub_matrix(matrix):
 
     for i in range(half_n):
         for j in range(half_n):
-            m12[i][j] = matrix[i][j+half_n]
+            m12[i][j] = matrix[i][j + half_n]
 
     for i in range(half_n):
         for j in range(half_n):
-            m21[i][j] = matrix[i+half_n][j]
+            m21[i][j] = matrix[i + half_n][j]
 
     for i in range(half_n):
         for j in range(half_n):
-            m22[i][j] = matrix[i+half_n][j+half_n]
+            m22[i][j] = matrix[i + half_n][j + half_n]
 
-    return m11, m12, m21, m22
+    return m11, m12, m21, m22, row_number, columum_number, n
 
+
+def _strassen(A, B):
+    if len(A) == 1:
+        return [[A[0][0] * B[0][0]]]
+    else:
+        a11, a12, a21, a22, a_row_number, a_columum_number, n = sub_matrix(A)
+        b11, b12, b21, b22, b_row_number, b_columum_number, n = sub_matrix(B)
+        s1 = matrix_minus(b12, b22)
+        s2 = matrix_add(a11, a12)
+        s3 = matrix_add(a21, a22)
+        s4 = matrix_minus(b21, b11)
+        s5 = matrix_add(a11, a22)
+        s6 = matrix_add(b11, b22)
+        s7 = matrix_minus(a12, a22)
+        s8 = matrix_add(b21, b22)
+        s9 = matrix_minus(a11, a21)
+        s10 = matrix_add(b11, b12)
+
+        p1 = _strassen(a11, s1)
+        p2 = _strassen(s2, b22)
+        p3 = _strassen(s3, b11)
+        p4 = _strassen(a22, s4)
+        p5 = _strassen(s5, s6)
+        p6 = _strassen(s7, s8)
+        p7 = _strassen(s9, s10)
+
+        c11 = matrix_add(matrix_minus(matrix_add(p5, p4), p2), p6)
+        c12 = matrix_add(p1, p2)
+        c21 = matrix_add(p3, p4)
+        c22 = matrix_minus(matrix_minus(matrix_add(p5, p1), p3), p7)
+
+        temp_array = [0] * n
+        matrix = [temp_array[:] for t in range(n)]
+        half_n = int(n / 2)
+
+        for i in range(half_n):
+            for j in range(half_n):
+                matrix[i][j] = c11[i][j]
+
+        for i in range(half_n):
+            for j in range(half_n):
+                matrix[i][j + half_n] = c12[i][j]
+
+        for i in range(half_n):
+            for j in range(half_n):
+                matrix[i + half_n][j] = c21[i][j]
+
+        for i in range(half_n):
+            for j in range(half_n):
+                matrix[i + half_n][j + half_n] = c22[i][j]
+        return matrix
 
 
 def strassen(A, B):
-    pass
+    a_row_number, a_column_number = get_matrix_size(A)
+    b_row_number, b_column_number = get_matrix_size(B)
+    c = _strassen(A, B)
+    if c[-1][-1] == 0:  # 说明有0填充
+        c = c[:a_row_number]
+        for i in range(a_row_number):
+            c[i] = c[i][:b_column_number]
+        return c
+    return c
 
 
 if __name__ == '__main__':
@@ -133,9 +240,7 @@ if __name__ == '__main__':
     B.append(row_B2)
     B.append(row_B3)
 
-    # print(A)
-    # print(B)
-    # C = matrix_multipy(A, B)
-    # print(C)
-    print(matrix_fillup(B))
-    print(sub_matrix(B))
+    print('A = {}'.format(A))
+    print('B = {}'.format(A))
+    print('A * B = {}'.format(matrix_multiply(A, B)))
+    print('A * B (by Strassen) = {}'.format(strassen(A, B)))
