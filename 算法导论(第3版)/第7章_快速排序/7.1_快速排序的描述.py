f5814930953675b4f852df5166f94106a6fe26e3
zhaopeng <418224361@qq.com>
2020-02-24 20:55:11 +0800

    完成快速排序

--- 算法导论(第3版)/第7章_快速排序/7.1_快速排序的描述.py
+++ 算法导论(第3版)/第7章_快速排序/7.1_快速排序的描述.py
@@ -18,16 +18,19 @@ def partition(array, p, r):
 
 def quick_sort(array):
     if len(array) == 1:
-        return array[0]
+        return array
     else:
         array, q = partition(array, 0, len(array))
-        left_array = quick_sort(array[0:q])
-        right_array = quick_sort(array[q:len(array)])
-        array = left_array.extend(right_array)
-        return array
+        # 一个不太好预防的边界条件
+        if q == 0:
+            return array
+        else:
+            left_array = quick_sort(array[0:q])
+            right_array = quick_sort(array[q:len(array)])
+            left_array.extend(right_array)
+        return left_array
 
 
 if __name__ == '__main__':
     A = [2, 8, 7, 1, 3, 5, 6, 4]
-    # print(partition(A, 0, len(A)))
-    quick_sort(A)
+    print(quick_sort(A))
