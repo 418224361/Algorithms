aeaacd0540617a1537085beb6bf0ef3378c29292
zhaopeng <418224361@qq.com>
2020-03-08 14:40:17 +0800

    增加

--- Introduction_To_Algorithms/第8章_线性时间排序/8.3_基数排序.py
+++ Introduction_To_Algorithms/第8章_线性时间排序/8.3_基数排序.py
@@ -1,8 +1,12 @@
-
-
+def radix_sorting(array):
+    # 检查array的元素长度是否一样
+    d = len(array[0])
+    for item in array:
+        if len(item) != d:
+            raise ValueError('{}的长度不一样'.format(item))
 
 
 
 alphabeta = ['cow', 'dog', 'sea', 'rug', 'row', 'mob', 'box', 'tab', 'bar', 'ear', 'tar', 'dig', 'big', 'tea',
-                 'now', 'fox']
+             'now', 'fox']
 print(list(zip(item for item in alphabeta)))
