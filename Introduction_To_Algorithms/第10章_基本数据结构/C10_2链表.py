71f618eb7bb4c4645d071addc71eec8a346605a3
418224361 <418224361@qq.com>
2020-03-27 17:14:45 +0800

    增加习题

--- Introduction_To_Algorithms/第10章_基本数据结构/C10_2链表.py
+++ Introduction_To_Algorithms/第10章_基本数据结构/C10_2链表.py
@@ -14,6 +14,8 @@ class Link:
         x = self.head
         while x is not None and x.key != key:
             x = x.next
+        if x is None:
+            raise ValueError('{} not exists'.format(key))
         return x
 
     def insert(self, node):
@@ -34,5 +36,79 @@ class Link:
     def delete_key(self, key):
         node = self.search(key)
         if not node:
-            raise ValueError('{} not exist'.format(key))
+            raise ValueError('{} not exists'.format(key))
         self.delete_node(node)
+
+    def __len__(self):
+        size = 0
+        x = self.head
+        while x is not None:
+            size += 1
+            x = x.next
+        return size
+
+
+class SingleLink:
+    def __init__(self, head=None):
+        self.head = head
+
+    def search(self, key):
+        x = self.head
+        while x is not None and x.key != key:
+            x = x.next
+        return x
+
+    def insert(self, node):
+        node.next = self.head
+        self.head = node
+
+    def delete_node(self, node):
+        x = self.head
+        if x == node:
+            self.head = x.next
+        else:
+            while x is not None and x.next != node:
+                x = x.next
+            if x is None:
+                raise IndexError('{} not exists'.format(node.key))
+            x.next = node.next
+
+    def delete_key(self, key):
+        x = self.head
+        if x.key == key:
+            self.head = x.next
+        else:
+            while x is not None and x.next.key != key:
+                x = x.next
+            if x is None:
+                raise IndexError('{} not exits'.format(key))
+            x.next = x.next.next
+
+
+# 练习10.2-2 用单链表实现一个栈，要求push和pop操作时间是O(1)
+class LinkStack(SingleLink):
+    def __init__(self):
+        super().__init__()
+
+    def push(self, node):
+        return super().insert(node)
+
+    def pop(self):
+        x = self.head
+        super().delete_node(x)
+        return x
+
+
+if __name__ == '__main__':
+    link = Link()
+    node1 = Node(9, '9')
+    node2 = Node(16, '16')
+    node3 = Node(4, '4')
+    node4 = Node(1, '1')
+    node5 = Node(25, '25')
+    link.insert(node1)
+    link.insert(node2)
+    link.insert(node3)
+    link.insert(node4)
+    for i in [9, 16, 4, 1, 25]:
+        print(link.search(i).value)
